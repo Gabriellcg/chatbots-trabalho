9666bfa92324928c25f9488a7d9f9362cecc9a9b
Cauã Silva <caua.silva@cali.com.br>
2025-09-22 16:37:48 -0300

    exe 1 finalizado, falta o readme

--- chatbot_faq/main.py
+++ chatbot_faq/main.py
@@ -22,23 +22,3 @@ while True:
         break
     resposta = chatbot.get_response(pergunta)
     print("Bot:", resposta)
-
-
-#from chatterbot.trainers import ListTrainer
-#from chatterbot import ChatBot
-
-#faq_bot = ChatBot('FAQBot', read_only=True)
-#trainer = ListTrainer(faq_bot)
-
-#with open ('base_faq.yml', 'r', encoding='utf-8') as f:
-#    linhas = f.readline()
-
-#    pares = [linha.strip() for linha in linhas if linha.strip()]
-
-#    trainer.train(pares)
-
-#while True:
-#    pergunta = input("Você: ")
-
-#    resposta = faq_bot.generate_response(pergunta)
-#    print("Bot>: ", resposta)
